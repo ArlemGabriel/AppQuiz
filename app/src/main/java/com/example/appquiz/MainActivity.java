b611d1aa16d3431f7bc24568d730ac27f91b0709
Gabriel <jogabra16@gmail.com>
2019-04-03 21:36:10 -0600

    Se realiza la carga del HTML de manera exitosa

--- app/src/main/java/com/example/appquiz/MainActivity.java
+++ app/src/main/java/com/example/appquiz/MainActivity.java
@@ -1,13 +1,76 @@
 package com.example.appquiz;
-
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Best practice: descargar contenido en un thread distinto al thread de UI
+    // Primero: tipo de variable que le vamos a mandar a la clases para decirle qué hacer. En este caso, un url
+    // Segundo: nombre de método que podríamos usar para mostrar el progreso de la tarea
+    // Tercero: tipo de variable que va a ser retornado por la clase
+    public class DownloadTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+
+            String result = "";
+            URL url;
+
+            // Agregar permiso en AndroidManifest.xml
+            HttpURLConnection urlConnection = null;
+
+            try {
+                url = new URL(urls[0]);
+
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream inputStream = urlConnection.getInputStream();
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+
+                // Esto es muy estilo C
+                // Se lee un caracter a la vez (como cuando se hace gets() en C o C++)
+                int data = inputStreamReader.read();
+                while (data != -1){
+                    char character = (char)data;
+                    result += character;
+                    data = inputStreamReader.read();
+                }
+
+                return result;
+            }
+            catch (Exception e){
+                e.printStackTrace();
+                return "Error";
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        cargarHTML();
+
+    }
+    public void cargarHTML(){
+        DownloadTask downloadTask = new DownloadTask();
+        String result = null;
+        try {
+            result = downloadTask.execute("https://listas.20minutos.es/lista/cual-es-tu-personaje-favorito-de-nanatsu-no-taizai-421522").get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        Log.i("Result", result);
     }
 }
+
