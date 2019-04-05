057a3f90800b89cfb8fd94c7e3786247a40424b9
Gabriel <jogabra16@gmail.com>
2019-04-04 18:42:35 -0600

    Se agregan los nombres de personajes y la imagen elegidas en la parte gráfica

--- app/src/main/java/com/example/appquiz/MainActivity.java
+++ app/src/main/java/com/example/appquiz/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.appquiz;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import org.jsoup.Jsoup;
@@ -12,9 +16,11 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Random;
@@ -28,6 +34,8 @@ public class MainActivity extends AppCompatActivity {
     // Tercero: tipo de variable que va a ser retornado por la clase
     ArrayList<String> nombresPersonajes = new ArrayList<>();
     ArrayList<String> imagenesPersonajes = new ArrayList<>();
+    int personajeActual;
+    String personajeActualStr;
     public class DownloadTask extends AsyncTask<String, Void, String> {
 
         @Override
@@ -61,6 +69,29 @@ public class MainActivity extends AppCompatActivity {
                 e.printStackTrace();
                 return "Error";
             }
+        }
+    }
+    public class ImageDownloader extends AsyncTask<String, Void, Bitmap> {
+
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+
+
+            try {
+                URL url = new URL(urls[0]);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+                connection.connect();
+                InputStream inputStream = connection.getInputStream();
+                Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                return bitmap;
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
         }
     }
 
@@ -101,17 +132,25 @@ public class MainActivity extends AppCompatActivity {
             nombresPersonajes.add(personajes.get(i).attr("alt"));
             imagenesPersonajes.add(personajes.get(i).attr("data-original"));
         }
-        elegirPersonajes();
-        elegirPersonajes();
-        elegirPersonajes();
-        elegirPersonajes();
-        elegirPersonajes();
-        elegirPersonajes();
-        elegirPersonajes();
-
+        iniciarJuego();
     }
-    public void iniciarJuego(View view){
-        //int personaje2=
+    public void iniciarJuego(){
+        ArrayList<Integer> personajesElegidos = new ArrayList<Integer>();
+        ArrayList<String> personajes= new ArrayList<String>();
+
+        personajesElegidos = elegirPersonajesAleatorios();
+        personajes = obtenerNombresPersonajes(personajesElegidos);
+
+        establecerTextoBotones(personajes);
+
+        int largoPersonajes = personajesElegidos.size()-1;
+        int posicionPersonaje = numeroAleatorio(largoPersonajes);
+        personajeActual = personajesElegidos.get(posicionPersonaje);
+        personajeActualStr = personajes.get(posicionPersonaje);
+
+        String imagenPersonaje = obtenerImagenPersonaje(personajeActual);
+        establecerImagenPersonaje(imagenPersonaje);
+
     }
     public int numeroAleatorio(int pLargoLista){
         //generador de numeros aleatorios
@@ -120,7 +159,8 @@ public class MainActivity extends AppCompatActivity {
         int numero = generadorAleatorios.nextInt(pLargoLista+1);
         return numero;
     }
-    public void elegirPersonajes(){
+    public ArrayList<Integer> elegirPersonajesAleatorios(){
+        ArrayList<Integer> personajesEscogidos = new ArrayList<Integer>();
         int largoPersonajes = nombresPersonajes.size()-1;
         int personaje1=numeroAleatorio(largoPersonajes);
         int personaje2=numeroAleatorio(largoPersonajes);
@@ -141,10 +181,80 @@ public class MainActivity extends AppCompatActivity {
                 personaje4=numeroAleatorio(largoPersonajes);
             }
         }
-        Log.i("PERSONAJE1:",String.valueOf(personaje1));
-        Log.i("PERSONAJE2:",String.valueOf(personaje2));
-        Log.i("PERSONAJE3:",String.valueOf(personaje3));
-        Log.i("PERSONAJE4:",String.valueOf(personaje4));
+        personajesEscogidos.add(personaje1);
+        personajesEscogidos.add(personaje2);
+        personajesEscogidos.add(personaje3);
+        personajesEscogidos.add(personaje4);
+
+        return personajesEscogidos;
+    }
+    public ArrayList<String> obtenerNombresPersonajes(ArrayList<Integer> personajesElegidos){
+        ArrayList<String> personajes= new ArrayList<String>();
+        int largoEscogidos = personajesElegidos.size()-1;
+        for(int i=0;i<=largoEscogidos;i++){
+            int posicion = personajesElegidos.get(i);
+            personajes.add(nombresPersonajes.get(posicion));
+        }
+        return personajes;
+    }
+    public String obtenerImagenPersonaje(int posicionImagen){
+        String imagenPersonaje = imagenesPersonajes.get(posicionImagen);
+        return  imagenPersonaje;
+    }
+    public void establecerTextoBotones(ArrayList<String> personajes){
+        Button btnOp1 = (Button)findViewById(R.id.btnOpcion1);
+        Button btnOp2 = (Button)findViewById(R.id.btnOpcion2);
+        Button btnOp3 = (Button)findViewById(R.id.btnOpcion3);
+        Button btnOp4 = (Button)findViewById(R.id.btnOpcion4);
+
+
+        btnOp1.setText(personajes.get(0));
+        btnOp2.setText(personajes.get(1));
+        btnOp3.setText(personajes.get(2));
+        btnOp4.setText(personajes.get(3));
+    }
+    public void establecerImagenPersonaje(String imagenPersonaje){
+        ImageView imagen = (ImageView)findViewById(R.id.imgPersonaje);
+        ImageDownloader imageDownloader = new ImageDownloader();
+        try {
+            Bitmap image = imageDownloader.execute(imagenPersonaje).get();
+            imagen.setImageBitmap(image);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+    public void btnOp1Clicked(View view){
+        Button btnOp1 = (Button)findViewById(R.id.btnOpcion1);
+        if(btnOp1.getText() == personajeActualStr){
+            btnOp1.setBackgroundResource(R.drawable.correcto);
+        }else{
+            btnOp1.setBackgroundResource(R.drawable.incorrecto);
+        }
+    }
+    public void btnOp2Clicked(View view){
+        Button btnOp2 = (Button)findViewById(R.id.btnOpcion2);
+        if(btnOp2.getText() == personajeActualStr){
+            btnOp2.setBackgroundResource(R.drawable.correcto);
+        }else{
+            btnOp2.setBackgroundResource(R.drawable.incorrecto);
+        }
+    }
+    public void btnOp3Clicked(View view){
+        Button btnOp3 = (Button)findViewById(R.id.btnOpcion3);
+        if(btnOp3.getText() == personajeActualStr){
+            btnOp3.setBackgroundResource(R.drawable.correcto);
+        }else{
+            btnOp3.setBackgroundResource(R.drawable.incorrecto);
+        }
+    }
+    public void btnOp4Clicked(View view){
+        Button btnOp4 = (Button)findViewById(R.id.btnOpcion4);
+        if(btnOp4.getText() == personajeActualStr){
+            btnOp4.setBackgroundResource(R.drawable.correcto);
+        }else{
+            btnOp4.setBackgroundResource(R.drawable.incorrecto);
+        }
     }
 }
 
